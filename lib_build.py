b33fcca7df7dfc2fa41a68913eda21a9fa83f612
jpedley <james.Bennion-Pedley@dyson.com>
2024-07-29 14:36:53 +0800

    Small functionality upgrade: allow getting Git metadata from a subdirectory

--- lib_build.py
+++ lib_build.py
@@ -4,6 +4,7 @@ Script for extracting version control information at compile time
 Runs as a PlatformIO library build hook. Can also be ported to other IDEs, or
 run manually if another means of modifying the environment is provided.
 """
+
 import datetime
 import getpass
 
@@ -25,9 +26,9 @@ __version__ = "1.0.0"
 compile_time = datetime.datetime.now(tz=None).strftime("%Y-%m-%dT%H:%MZ")
 
 # Check if in a Git repository: if not, populate with placeholder
-path = env['PROJECT_DIR']
+path = env["PROJECT_DIR"]
 try:
-    g = git.Repo(path)
+    g = git.Repo(path, search_parent_directories=True)
     commit_hash = g.head.object.hexsha
     short_hash = g.git.rev_parse(commit_hash, short=7)[0:10]
     is_dirty = g.is_dirty()
@@ -54,17 +55,13 @@ print("************************************************")
 # Append macros to global build environment
 env.Append(
     CPPDEFINES=[
-        ('__VCS_COMPILE_TIME',  "\\\"" + compile_time + "\\\""),
-        ('__VCS_SHORT_HASH', "\\\"" + short_hash + "\\\""),
-        ('__VCS_IS_DIRTY', '1' if is_dirty else '0'),
-        ('__VCS_TAG_DESCRIBE', "\\\"" + tag_describe + "\\\""),
-        ('__VCS_LAST_AUTHOR', "\\\"" + author + "\\\"")
+        ("__VCS_COMPILE_TIME", '\\"' + compile_time + '\\"'),
+        ("__VCS_SHORT_HASH", '\\"' + short_hash + '\\"'),
+        ("__VCS_IS_DIRTY", "1" if is_dirty else "0"),
+        ("__VCS_TAG_DESCRIBE", '\\"' + tag_describe + '\\"'),
+        ("__VCS_LAST_AUTHOR", '\\"' + author + '\\"'),
     ]
 )
 
 # Ensure that the VCS block does not get optimised out of the final binary
-env.Append(
-  LINKFLAGS=[
-      "-Wl,--undefined=vcs"
-  ]
-)
+env.Append(LINKFLAGS=["-Wl,--undefined=vcs"])
